4618dbcc7dbce510ea2f0d934f7300b784b61ab4
Patrick Armentrout <patrick.armentrout@gmail.com>
2023-02-19 16:32:07 -0700

    Check in evolution, small bug

--- src/PokemonBattle.js
+++ src/PokemonBattle.js
@@ -10,6 +10,7 @@ export default class PokemonBattle extends React.Component {
             pokemon2: {},
             pokemon1PageData: {
                 health: 0,
+                lostHealth: 0,
                 specialAttackUsed: false,
                 eliminated: false,
                 turn: true,
@@ -18,6 +19,7 @@ export default class PokemonBattle extends React.Component {
             },
             pokemon2PageData: {
                 health: 0,
+                lostHealth: 0,
                 specialAttackUsed: false,
                 eliminated: false,
                 turn: true,
@@ -43,9 +45,13 @@ export default class PokemonBattle extends React.Component {
         // Now we need to iteratively recurse the chain to find out where we are and what is next
         if (evolveData && evolveData.chain && evolveData.chain.species) {
             const firstLayer = evolveData.chain.species.name;
-            const secondLayer = evolveData.chain.evolves_to[0].species.name;
-            const thirdLayer = evolveData.chain.evolves_to[0].evolves_to.length ?
-                evolveData.chain.evolves_to[0].evolves_to[0].species.name : '';
+            const secondLayer = evolveData.chain.evolves_to.length ?
+                evolveData.chain.evolves_to[0].species.name : '';
+            let thirdLayer = '';
+            if (secondLayer) {
+                thirdLayer = evolveData.chain.evolves_to[0].evolves_to.length ?
+                    evolveData.chain.evolves_to[0].evolves_to[0].species.name : '';
+            }
         
 
             switch (pokemon) {
@@ -57,29 +63,29 @@ export default class PokemonBattle extends React.Component {
                     break;
                 case thirdLayer:
                     {
-                        // Check for varieties and return one of the varieties
-                        let varieties = species.varieties.filter(x => x.pokemon.name !== pokemon);
-                        console.log(varieties);
-                        if (varieties.length) {
-                            const random = Math.floor(Math.random() * varieties.length);
-                            evolvesTo = varieties[random].pokemon.name;
-                        }
+
+                        // // Check for varieties and return one of the varieties
+                        // let varieties = species.varieties.filter(x => x.pokemon.name !== pokemon);
+                        // if (varieties.length) {
+                        //     const random = Math.floor(Math.random() * varieties.length);
+                        //     evolvesTo = varieties[random].pokemon.name;
+                        // }
                     }
                     break;
             }
            
         }
-        
+
         return evolvesTo;
     }
 
-    getPokemonData = async (pokemon, player) => {
+    getPokemonData = async (pokemon, player, lostHealth) => {
         // Get details
         const getUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         let res = await fetch(getUrl);
         let pokemonData = await res.json();
 
-        const totalHealth = pokemonData.stats[0].base_stat + pokemonData.stats[2].base_stat; // HP + Defense
+        const totalHealth = pokemonData.stats[0].base_stat + pokemonData.stats[2].base_stat - lostHealth; // HP + Defense
         const evolvesTo = await this.getEvolutionData(pokemonData.name);
         console.log(evolvesTo);
 
@@ -95,7 +101,9 @@ export default class PokemonBattle extends React.Component {
                         health: totalHealth, 
                         specialAttackUsed: false,
                         turn: true,
-                        imageUrl
+                        lostHealth,
+                        imageUrl,
+                        evolvesTo
                     }
                 }
             )
@@ -108,7 +116,9 @@ export default class PokemonBattle extends React.Component {
                         health: totalHealth, 
                         specialAttackUsed: false,
                         turn: true,
-                        imageUrl
+                        lostHealth,
+                        imageUrl,
+                        evolvesTo
                     }
                 }
             )
@@ -117,8 +127,12 @@ export default class PokemonBattle extends React.Component {
     }
     async componentDidMount() {
         const { player1, player2 } = this.props;
-        await this.getPokemonData(player1, 1);
-        await this.getPokemonData(player2, 2);
+        await this.getPokemonData(player1, 1, 0, null);
+        await this.getPokemonData(player2, 2, 0, null);
+    }
+
+    evolveNotAttack = async (playerInitiated, evolvesTo, lostHealth) => {
+        await this.getPokemonData(evolvesTo, playerInitiated, lostHealth);
     }
     
 
@@ -134,8 +148,9 @@ export default class PokemonBattle extends React.Component {
         
         if (playerInitiated === 1) {
             attackAmount = isSpecial ? this.state.pokemon1.stats[3].base_stat : this.state.pokemon1.stats[1].base_stat;
-            let newHealth = this.state.pokemon2PageData.health - attackAmount;
             
+            let newHealth = this.state.pokemon2PageData.health - attackAmount;
+            let lostHealth = this.state.pokemon2PageData.lostHealth + attackAmount;
 
             if (newHealth < 0 ) { 
                 newHealth = 0;
@@ -147,13 +162,17 @@ export default class PokemonBattle extends React.Component {
                         health: this.state.pokemon1PageData.health,
                         specialAttackUsed: isSpecial,
                         turn: false,
-                        imageUrl: imageUrl1
+                        imageUrl: imageUrl1,
+                        lostHealth: this.state.pokemon1PageData.lostHealth,
+                        evolvesTo: this.state.pokemon1PageData.evolvesTo
                     },
                     pokemon2PageData: {
                         health: newHealth,
                         specialAttackUsed: this.state.pokemon2PageData.specialAttackUsed,
                         turn: true,
-                        imageUrl: imageUrl2
+                        imageUrl: imageUrl2,
+                        lostHealth,
+                        evolvesTo: this.state.pokemon2PageData.evolvesTo
                     }
                 }
             )
@@ -162,6 +181,8 @@ export default class PokemonBattle extends React.Component {
         if (playerInitiated === 2) {
             attackAmount = isSpecial ? this.state.pokemon2.stats[3].base_stat : this.state.pokemon2.stats[1].base_stat;
             let newHealth = this.state.pokemon1PageData.health - attackAmount;
+            let lostHealth = this.state.pokemon1PageData.lostHealth + attackAmount;
+
             if (newHealth < 0 ) { 
                 newHealth = 0;
                 this.props.onWinnerSelected(this.state.pokemon2);
@@ -172,13 +193,17 @@ export default class PokemonBattle extends React.Component {
                         health: newHealth, 
                         specialAttackUsed: this.state.pokemon1PageData.specialAttackUsed,
                         turn: true,
-                        imageUrl: imageUrl1
+                        imageUrl: imageUrl1,
+                        lostHealth,
+                        evolvesTo: this.state.pokemon1PageData.evolvesTo
                     },
                     pokemon2PageData: {
                         health: this.state.pokemon2PageData.health,
                         specialAttackUsed: isSpecial,
                         turn: false,
-                        imageUrl: imageUrl2
+                        imageUrl: imageUrl2,
+                        lostHealth: this.state.pokemon2PageData.lostHealth,
+                        evolvesTo: this.state.pokemon2PageData.evolvesTo
                     }
                 }
             )
@@ -207,6 +232,7 @@ export default class PokemonBattle extends React.Component {
                     </ul>
                     <button className={styles.niceButton} onClick={() => this.attackPlayer(1,false)} disabled={!pokemon1PageData.turn}>Attack! ({pokemon1 && pokemon1.stats && pokemon1.stats[1].base_stat})</button>
                     <button className={styles.niceButton} onClick={() => this.attackPlayer(1,true)} disabled={!pokemon1PageData.turn || pokemon1PageData.specialAttackUsed}>Special Attack! ({pokemon1 && pokemon1.stats && pokemon1.stats[3].base_stat})</button>
+                    <button className={styles.niceButton} onClick={() => this.evolveNotAttack(1, pokemon1PageData.evolvesTo, pokemon1PageData.lostHealth)} disabled={pokemon1PageData.evolvesTo === '' || !pokemon1PageData.turn}>Evolve</button>
                 </div>
                 <div className={styles.card}>
                     <h2>{ pokemon2.name }</h2>
@@ -224,6 +250,7 @@ export default class PokemonBattle extends React.Component {
                     </ul>
                     <button className={styles.niceButton} onClick={() => this.attackPlayer(2,false)} disabled={!pokemon2PageData.turn}>Attack! ({pokemon2 && pokemon2.stats && pokemon2.stats[1].base_stat})</button>
                     <button className={styles.niceButton} onClick={() => this.attackPlayer(2,true)} disabled={!pokemon2PageData.turn || pokemon2PageData.specialAttackUsed}>Special Attack! ({pokemon2 && pokemon2.stats && pokemon2.stats[3].base_stat})</button>
+                    <button className={styles.niceButton} onClick={() => this.evolveNotAttack(2, pokemon2PageData.evolvesTo, pokemon2PageData.lostHealth)} disabled={pokemon2PageData.evolvesTo === '' || !pokemon2PageData.turn}>Evolve</button>
                 </div>
             </div>
         )
